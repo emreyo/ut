6bd180a1d840189da62e3d7e262fc62d273cb4c5
Emre Y <emreyond@gmail.com>
2014-11-08 19:09:00 +0200

    check libav-tools

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [ "$(whoami)" != "root" ]; then
-	echo "error. need root!"
+	echo "* error - need root"
+	exit 1
 fi
 
 if [ ! -d "$HOME/bin" ]; then
@@ -12,6 +13,8 @@ if [ -z `which youtube-dl` ]; then
 	sudo curl https://yt-dl.org/downloads/2014.09.22/youtube-dl -o /usr/local/bin/youtube-dl
 	sudo chmod a+x /usr/local/bin/youtube-dl
 fi
+
+[ -z "$(dpkg --get-selections | grep libav-tools)" ] && sudo apt-get install -y libav-tools
 
 cp ./ut "$HOME/bin/ut"
 chmod +x "$HOME/bin/ut"
